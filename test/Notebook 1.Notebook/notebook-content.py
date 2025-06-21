19e07e717c0c2d7968342c2297a63a8438df8e87
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-06-21 20:35:04 -0300

    Committing 1 item from workspace 235737c8-d501-4608-a4b2-4048a6569358

---
+++ test/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "9542ca62-8199-4b77-8bc4-4eebe18632cd",
+# META       "default_lakehouse_name": "LakeTest",
+# META       "default_lakehouse_workspace_id": "235737c8-d501-4608-a4b2-4048a6569358",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "9542ca62-8199-4b77-8bc4-4eebe18632cd"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
